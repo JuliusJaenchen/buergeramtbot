6763bbb5b98528acd5f6c1fdd6e5fae5784ddf48
Julius Jänchen <julius.jaenchen@check24.de>
2024-04-09 17:46:23 +0200

    cleaned code and added some documentation

--- main.go
+++ main.go
@@ -5,35 +5,23 @@ import (
 	"log"
 	"net/http"
 	"net/http/cookiejar"
-	"net/url"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+// This page is the entry link to search for home-regestration
+// We need to call it first to get valid cookies
 var entryURL = "https://service.berlin.de/terminvereinbarung/termin/all/120686/"
+
+// TODO place current UNIX timestamp in slug. The current one is still hardcoded...
 var appointmentURL = "https://service.berlin.de/terminvereinbarung/termin/day/1714428000/"
 
 func main() {
-	cookies := make([]*http.Cookie, 0)
-	/*
-		cookies = append(cookies, &http.Cookie{Name: "zmsappointment-session", Value: "inProgress"})
-		cookies = append(cookies, &http.Cookie{Name: "Zmsappointment", Value: "nhbr3g3lmklo925mvto4m8pg7j"})
-		cookies = append(cookies, &http.Cookie{Name: "wt_rla", Value: "102571513503709%2C64%2C1712675202268"})
-	*/
-	baseURL, _ := url.Parse("https://service.berlin.de")
 	jar, _ := cookiejar.New(nil)
-	jar.SetCookies(baseURL, cookies)
-	httpClient := &http.Client{
-		Jar: jar,
-	}
-	httpClient.Jar.SetCookies(baseURL, cookies)
+	httpClient := &http.Client{Jar: jar}
 
-	req, err := http.NewRequest("GET", entryURL, nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	addHeaders(req)
-
+	// make first request to get session-cookies
+	req := createRequest(entryURL)
 	res, err := httpClient.Do(req)
 	if err != nil {
 		log.Fatal(err)
@@ -43,11 +31,8 @@ func main() {
 		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 	}
 
-	req, err = http.NewRequest("GET", appointmentURL, nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	addHeaders(req)
+	// make second request to get HTML page with appointments
+	req = createRequest(appointmentURL)
 	res, err = httpClient.Do(req)
 	if err != nil {
 		log.Fatal(err)
@@ -57,6 +42,7 @@ func main() {
 		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 	}
 
+	// parse HTML to get open appointments
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		log.Fatal(err)
@@ -70,11 +56,16 @@ func main() {
 	bookableDataPoints := doc.Find("td.buchbar")
 	// TODO parse dates out of links for more details
 	if len(bookableDataPoints.Nodes) > 0 {
-		fmt.Print("Sucess! ")
+		fmt.Print("Success! ")
 	}
 	fmt.Printf("Found %v days with open slots\n", len(bookableDataPoints.Nodes))
 }
 
-func addHeaders(req *http.Request) {
+func createRequest(url string) *http.Request {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
+	return req
 }
